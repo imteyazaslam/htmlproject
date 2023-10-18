e62e5c7f887654e1cef3f2094a8d8eede506ad5c
imteyaz <imteyazaslam123@gmail.com>
2023-10-18 10:33:50 +0530

    count of letter

---
+++ JS/countofLetter.js
@@ -0,0 +1,12 @@
+function letterCount(str, c) {
+    var count=0;
+    str=str.toLowerCase(); //Case insensitive
+    for(var i=0;i<str.length;i++){
+    if(str.charAt(i)==c){
+    count++; //Increment count
+    }
+    }
+    return count;
+   }
+   console.log(letterCount("Connect",'c')) // 2
+   console.log(letterCount("first person",'s')) // 
