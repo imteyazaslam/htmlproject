e2e05801ba82693feb7d80edba13a7307ebcaa7a
imteyaz <imteyazaslam123@gmail.com>
2023-10-19 10:27:10 +0530

    recursion function

---
+++ JS/DSA/recursionfn.js
@@ -0,0 +1,14 @@
+function printFun(test)
+{
+if (test < 1)
+return;
+else {
+console.log(test + " ");
+printFun(test - 1); // statement 2
+console.log(test + " ");
+return;
+}
+}
+// Driver code
+let test = 3;
+printFun(test);
