318161ed3b195d59ea32877aa7276b4927ced2f0
imteyaz <imteyazaslam123@gmail.com>
2023-10-22 12:01:22 +0530

    area of triangle

---
+++ JS/areaofTriangle.js
@@ -0,0 +1,9 @@
+//JavaScript Program To Calculate The Area of a Triangle
+var base = parseInt(prompt("Enter the base: "));
+var height = parseInt(prompt("Enter the height: "));
+//Calculating the area
+var area = (base * height) / 2;
+//Display Output
+console.log("Base: " + base);
+console.log("Height: " + height);
+console.log("The area of the triangle is " + area);
