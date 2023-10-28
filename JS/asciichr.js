020cf0b9d172f4bbe8904f9bcee78e4bf9f8f697
imteyaz <imteyazaslam123@gmail.com>
2023-10-28 19:08:06 +0530

    ascii value

---
+++ JS/asciichr.js
@@ -0,0 +1,8 @@
+// Find the ASCII value of a character Programs
+
+const character= prompt('Enter a character: ');
+
+// convert into ASCII value
+const res = character.charCodeAt(0);
+
+console.log("The ASCII value is: " , res);
