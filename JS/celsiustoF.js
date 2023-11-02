2c7c6a72ce237860c324be7dd87d542d0dcba23a
imteyaz <imteyazaslam123@gmail.com>
2023-11-02 18:50:47 +0530

    celsius to faren

---
+++ JS/celsiustoF.js
@@ -0,0 +1,6 @@
+function calFahrenheit(cel) {
+    return (cel*9/5)+32; //Conversion formula
+   }
+   console.log(calFahrenheit(0))
+   console.log(calFahrenheit(20)) 
+   console.log(calFahrenheit(40)) 
